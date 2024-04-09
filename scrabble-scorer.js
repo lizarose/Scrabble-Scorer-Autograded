58171a1adccec06b4ac17d6b57f6b92530c1174c
Elizabeth Levinson <lizarose266@gmail.com>
2024-04-09 16:12:57 -0500

    Task 1 Completed

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,7 +33,11 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   console.log("Let's play some Scrabble!\n");
+
+   let word = input.question("Enter a word to score: ");
+   console.log(oldScrabbleScorer(word));
+   return oldScrabbleScorer(word);
 };
 
 let simpleScorer;
